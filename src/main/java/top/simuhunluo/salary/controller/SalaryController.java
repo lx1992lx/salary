d11c67824e64bf2adb5595ae40f93940c8ab5c43
simuhunluo <simuhunluo@outlook.com>
2018-07-05 19:56:00 +0800

    写了company控制类的demo

---
+++ src/main/java/top/simuhunluo/salary/controller/SalaryController.java
@@ -0,0 +1,11 @@
+package top.simuhunluo.salary.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RestController;
+
+@ApiOperation(value = "salary接口",notes = "getSalaryList/getSalaryById")
+@RestController
+public class SalaryController {
+    //TODO 定时任务
+
+}
--- src/main/java/top/simuhunluo/salary/exception/SalaryException.java
+++ src/main/java/top/simuhunluo/salary/exception/SalaryException.java
@@ -2,11 +2,13 @@ package top.simuhunluo.salary.exception;
 
 import top.simuhunluo.salary.common.ErrorCode;
 
+// TODO 自定义异常
 public class SalaryException extends RuntimeException {
     private ErrorCode errorCode;
 
     public SalaryException(ErrorCode errorCode) {
         super(errorCode.toString());
-        this.errorCode=errorCode;
+        this.errorCode = errorCode;
+
     }
 }
